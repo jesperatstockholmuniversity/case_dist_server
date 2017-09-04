a839b9e187f0e469d549caa0f2829cd8820a10c3
Anton Wiklund <ixuz@antwik-pc.localdomain>
2017-09-04 16:05:40 +0200

    Changed package for http requests

--- test/test.js
+++ test/test.js
@@ -1,108 +1,160 @@
 var config = require('config');
 var assert = require('assert');
+var request = require('request');
 
-var clients = require('restify-clients');
-
-var client = clients.createJsonClient({
-  'url': 'http://localhost:' + config.get('General.server.port')
-});
+var hostname = 'http://localhost:' + config.get('General.server.port');
 
 describe('Testing the REST API', function() {
   it('should successfully seed the database', function(done) {
-    client.get('/seed', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Success');
-      assert.equal(obj.message, '');
+    request(hostname+'/seed', function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(error, null);
+      assert.equal(response.statusCode, 200);
+      assert.equal(response.json.status, 'Success');
       done();
     });
   });
 
   it('should not find a distribution for the uuid "test-uuid"', function(done) {
-    client.get('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Distribution not found');
+    request(hostname+'/dist/test-uuid', function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(response.json.status, 'Error');
+      assert.equal(response.json.message, 'Distribution not found');
       done();
     });
   });
 
   it('should fail to create a distribution for the uuid "test-uuid" when the body is not an array', function(done) {
-    client.post('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Request body is not an array.');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: {this: "is not an array"}
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Error');
+      assert.equal(body.message, 'Request body is not an array.');
       done();
     });
   });
 
   it('should fail to create a distribution for the uuid "test-uuid" when the body array is empty', function(done) {
-    client.post('/dist/test-uuid', [], function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Request body array empty (length zero).');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: []
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Error');
+      assert.equal(body.message, 'Request body array empty (length zero).');
       done();
     });
   });
 
   it('should fail to create a distribution for the uuid "test-uuid" when the body array has an object without a "case_name"-property', function(done) {
-    client.post('/dist/test-uuid', [{}], function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Request body array item missing property "case_name"');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: [{}]
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Error');
+      assert.equal(body.message, 'Request body array item missing property "case_name"');
       done();
     });
   });
 
   it('should fail to create a distribution for the uuid "test-uuid" when the body array has an object that is a non-string "case_name"-property', function(done) {
-    client.post('/dist/test-uuid', [{"case_name": 123}], function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Request body array item property "case_name" is not a "string".');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: [{"case_name": 123}]
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Error');
+      assert.equal(body.message, 'Request body array item property "case_name" is not a "string".');
       done();
     });
   });
 
   it('should successfully create a distribution for the uuid "test-uuid" when all required values are set', function(done) {
-    client.post('/dist/test-uuid', [{"case_name": "test_case_name"}], function (err, req, res, obj) {
-      assert.equal(obj.status, 'Success');
-      assert.equal(obj.message, '');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: [{"case_name": "test_case_name"}]
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Success');
+      assert.equal(body.message, '');
       done();
     });
   });
-
+  
   it('should fail to create a distribution for the uuid "test-uuid" when the uuid already has a distribution', function(done) {
-    client.post('/dist/test-uuid', [{"case_name": "test_case_name"}], function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Distribution for uuid already exists');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'POST',
+      json: [{"case_name": "test_case_name"}]
+    },
+    function (error, response, body) {
+      assert.equal(body.status, 'Error');
+      assert.equal(body.message, 'Distribution for uuid already exists');
       done();
     });
   });
 
   it('should retrieve an existing distribution from the database', function(done) {
-    client.get('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Success');
-      assert.equal(typeof obj.message, 'object');
-      assert.equal(typeof obj.message[0], 'object');
-      assert.equal(typeof obj.message[0].case_name, 'string');
-      assert.equal(obj.message[0].case_name, 'test_case_name');
+    request(hostname+'/dist/test-uuid', function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(error, null);
+      assert.equal(response.statusCode, 200);
+      assert.equal(response.json.status, 'Success');
+      assert.equal(typeof response.json.message, 'object');
+      assert.equal(typeof response.json.message[0].case_name, 'string');
+      assert.equal(response.json.message[0].case_name, 'test_case_name');
       done();
     });
   });
 
   it('should delete an existing distribution from the database', function(done) {
-    client.del('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Success');
-      assert.equal(obj.message, '');
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'DELETE'
+    },
+    function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(response.json.status, 'Success');
+      assert.equal(response.json.message, '');
       done();
     });
   });
 
   it('should fail to retrieve a non-existing distribution from the database', function(done) {
-    client.get('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Distribution not found');
+    request(hostname+'/dist/test-uuid', function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(error, null);
+      assert.equal(response.statusCode, 404);
+      assert.equal(response.json.status, 'Error');
+      assert.equal(response.json.message, 'Distribution not found');
       done();
     });
   });
 
-  it('should fail to delete a non-existing distribution from the database', function(done) {
-    client.del('/dist/test-uuid', function (err, req, res, obj) {
-      assert.equal(obj.status, 'Error');
-      assert.equal(obj.message, 'Distribution not found');
+
+  it('should delete an existing distribution from the database', function(done) {
+    request({
+      url: hostname+'/dist/test-uuid',
+      method: 'DELETE'
+    },
+    function (error, response, body) {
+      response.json = JSON.parse(body);
+
+      assert.equal(response.json.status, 'Error');
+      assert.equal(response.json.message, 'Distribution not found');
       done();
     });
   });
